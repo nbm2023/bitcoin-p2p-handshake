161fa32d1e7fd77f1ace7b7c83685b5f1e9ca83f
nbm2023 <125922720+nbm2023@users.noreply.github.com>
2023-02-20 22:39:35 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use bitcoin::consensus::serialize;
-use bitcoin::network::message::{NetworkMessage, RawNetworkMessage};
-use bitcoin::network::message_network::VersionMessage;
+// use bitcoin::network::message::{NetworkMessage, RawNetworkMessage};
+// use bitcoin::network::message_network::VersionMessage;
 use std::io::Read;
 use std::io::Write;
 use std::io::{BufReader, BufWriter};
@@ -12,89 +12,92 @@ fn test_handshake() {
     handshake();
 }
 
+const PROTOCOL_VERSION: u32 = 70015;
+#[derive(Debug)]
+struct NetworkAddress {
+    services: u64,
+    ip: [u8; 16],
+    port: u16,
+}
+impl NetworkAddress {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(26);
+        bytes.extend_from_slice(&self.services.to_le_bytes());
+        bytes.extend_from_slice(&[0; 12]);
+        bytes.extend_from_slice(&self.ip);
+        bytes.extend_from_slice(&self.port.to_be_bytes());
+        bytes
+    }
+}
+#[derive(Debug)]
+struct VersionMessage {
+    version: u32,
+    services: u64,
+    timestamp: i64,
+    addr_recv: NetworkAddress,
+    addr_from: NetworkAddress,
+    nonce: u64,
+    user_agent: String,
+    start_height: i32,
+    relay: bool,
+}
+impl VersionMessage {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(85 + self.user_agent.len());
+        bytes.extend_from_slice(&self.version.to_le_bytes());
+        bytes.extend_from_slice(&self.services.to_le_bytes());
+        bytes.extend_from_slice(&self.timestamp.to_le_bytes());
+        bytes.extend_from_slice(&self.addr_recv.to_bytes());
+        bytes.extend_from_slice(&self.addr_from.to_bytes());
+        bytes.extend_from_slice(&self.nonce.to_le_bytes());
+        bytes.extend_from_slice(&(self.user_agent.len() as u8).to_le_bytes());
+        bytes.extend_from_slice(self.user_agent.as_bytes());
+        bytes.extend_from_slice(&self.start_height.to_le_bytes());
+        bytes.extend_from_slice(&(self.relay as u8).to_le_bytes());
+        bytes
+    }
+}
+
 fn handshake() {
-    // Connect to a Bitcoin node on the main network
-    let stream = TcpStream::connect("seed.bitcoin.sipa.be:8333").unwrap();
-    let mut reader = BufReader::new(&stream);
-    let mut writer = BufWriter::new(&stream);
+    // // Connect to a Bitcoin node on the main network
+    let mut stream = TcpStream::connect("seed.bitcoin.sipa.be:8333").unwrap();
 
     // Send a version message to the node
     let version_message = VersionMessage {
-        version: 70015,
-        services: 1.into(),
+        version: 80015,
+        services: 0,
         timestamp: chrono::Utc::now().timestamp() as i64,
-        receiver: bitcoin::network::address::Address::new(
-            &SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080),
-            0.into(),
-        ),
-        sender: bitcoin::network::address::Address::new(
-            &SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0),
-            0.into(),
-        ),
+        addr_recv: NetworkAddress {
+            services: 0,
+            ip: [0; 16],
+            port: 0,
+        },
+        addr_from: NetworkAddress {
+            services: 0,
+            ip: [0; 16],
+            port: 0,
+        },
         nonce: 0,
         user_agent: "/rust-bitcoin:0.1.0/".to_string(),
         start_height: 0,
         relay: false,
     };
 
-    let network_message = RawNetworkMessage {
-        magic: 0,
-        payload: NetworkMessage::Version(version_message),
-    };
+    let mut buf = Vec::new();
+    buf.extend_from_slice(b"0xD9B4BEF9");
+    buf.extend_from_slice(&"version".as_bytes());
+    buf.resize(12, 0);
+    buf.extend_from_slice(&(version_message.to_bytes().len() as u32).to_le_bytes());
+    buf.extend_from_slice(&0u32.to_le_bytes());
+    buf.extend_from_slice(&version_message.to_bytes());
+    let res = stream.write_all(&buf);
+    println!("Res: {:?}", res);
 
-    writer
-        .write_all(serialize(&network_message).as_slice())
-        .unwrap();
-    writer.flush().unwrap();
+    let mut reader = BufReader::new(&stream);
 
-    // Wait for a version message response from the node
-    loop {
-        let mut buf = [0u8; 1024];
-        let res = reader.read(&mut buf).unwrap();
-        println!("Res: {:?}", res); // ---> Always 0
-    }
+    let mut buf = [0u8; 1024];
+    let _ = reader.read(&mut buf).unwrap();
+    println!("Read res: {:?}", buf); // ---> Always empty
 
-    println!("Handshake completed successfully");
-}
-
-fn from_reader<R: std::io::Read>(reader: &mut R) -> Result<NetworkMessage, std::io::Error> {
-    // Read the first 4 bytes to get the message magic number.
-    let mut magic_bytes = [0u8; 4];
-    reader.read_exact(&mut magic_bytes)?;
-
-    // Read the next 12 bytes to get the message command.
-    let mut command_bytes = [0u8; 12];
-    reader.read_exact(&mut command_bytes)?;
-
-    // Parse the command bytes into a string and trim the null characters.
-    let command = std::str::from_utf8(&command_bytes)
-        .unwrap()
-        .trim_end_matches('\0')
-        .to_owned();
-
-    // Read the next 4 bytes to get the payload length.
-    let mut payload_length_bytes = [0u8; 4];
-    reader.read_exact(&mut payload_length_bytes)?;
-
-    // Parse the payload length bytes into a u32 integer.
-    let payload_length = u32::from_le_bytes(payload_length_bytes);
-
-    // Read the payload bytes.
-    let mut payload_bytes = vec![0u8; payload_length as usize];
-    reader.read_exact(&mut payload_bytes)?;
-
-    // Construct the network message.
-    let message = match command.as_str() {
-        "version" => NetworkMessage::Verack, //NetworkMessage::Version(VersionMessage::from_payload(&payload_bytes)?),
-        "verack" => NetworkMessage::Verack,
-        // Handle other message types here.
-        _ => {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                "invalid command",
-            ))
-        }
-    };
-
-    Ok(message)
+    println!("Why is the buf always empty :/ ");
 }
